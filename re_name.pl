c2c59fbc6be620d46be184aa6905db62cf771462
hechuweiran <fengfeng321@qq.com>
2017-03-01 17:47:27 +0800

    Create re_name.pl

---
+++ re_name.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl -w
+open(IN, $ARGV[0]) or die "";
+$/='>';
+<IN>;
+while (<IN>){
+	chomp;
+	my ($name,$seq) = split (/\n/,$_,2);
+	$name =~ s/\s+.*//g;
+	$seq  =~ s/\s+//;
+	print ">$name\n$seq\n";
+	}
+	close IN;
