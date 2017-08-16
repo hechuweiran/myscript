35feda7b3889e44d13177c28c018c10bc0815ab3
hechuweiran <fengfeng321@qq.com>
2017-08-16 21:53:39 +0800

    Update miss_rate.pl

--- miss_rate.pl
+++ miss_rate.pl
@@ -1,13 +1,15 @@
-#!perl -w 
+#!perl -w
 open (IN,$ARGV[0]) or die "";
 $/ = '>';
 <IN>;
 while(<IN>){
-	chomp;
-	my ($name,$seq) = split (/\n/,$_);
-	$all_seq = length $seq;
-	$lu = ($seq =~ tr/[atcg]/[ATCG]/);
-	$gers = $lu/$all_seq;
-	print "$name\t$gers\n";
-		}
+        chomp;
+        my ($name,$seq) = split (/\n/,$_);
+        $all_seq = length $seq;
+        $lu = ($seq =~ tr/[ATCG]/[atcg]/);
+        $gers = $lu/$all_seq;
+        $result = 1-$gers;
+        $result = sprintf("%.4f",$result);
+        print "$name\t$result\n";
+                }
 close IN;
